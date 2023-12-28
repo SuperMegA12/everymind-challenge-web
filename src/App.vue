bd486dd81a02fa8dc29148513d277f3417b139f9
SuperMEgA12 <francisquinholeite@gmail.com>
2023-12-28 17:46:34 -0300

    app bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
 v-app
+  v-app-bar(app color="purple" dark)
+    v-toolbar-title(class="text-h6 font-weight-medium text-center")
+      span.text-center Nunes Sports
   v-main
     product-table
 </template>
