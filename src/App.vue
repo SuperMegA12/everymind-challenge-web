4066097a3440674eab2bc7f00902f7cc16827e34
SuperMEgA12 <francisquinholeite@gmail.com>
2023-12-29 02:28:33 -0300

    div centered

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 v-app
   v-app-bar(app color="purple" dark)
-    v-toolbar-title(class="text-h6 font-weight-medium text-center")
-      span.text-center Nunes Sports
+    v-col(class="d-flex justify-space-around")
+      v-toolbar-title(class="text-h6 font-weight-medium text-center") Nunes Sports Web
   v-main
     product-table
 </template>
